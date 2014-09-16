8ce47c68bde34c149b9ca6ab1e03245cc3fa5540
deaddog <mikkelslsl@gmail.com>
2014-09-16 11:16:18 +0200

    Added documentation to the Heatmap type.

--- CityBikes/Library/Heatmap.cs
+++ CityBikes/Library/Heatmap.cs
@@ -2,6 +2,9 @@
 
 namespace Library
 {
+    /// <summary>
+    /// Represents a heat map in terms of a grid of 0-1 values.
+    /// </summary>
     public class Heatmap
     {
         private readonly int width, height;
@@ -20,20 +23,35 @@ namespace Library
             this.grid = new double[width, height];
         }
 
+        /// <summary>
+        /// Gets the value of a single cell in the heat map.
+        /// </summary>
+        /// <param name="x">The x coordinate of the cell.</param>
+        /// <param name="y">The y coordinate of the cell.</param>
+        /// <returns>A value in the range 0-1 representing the weight in the <c>[x, y]</c> cell.</returns>
         public double this[int x, int y]
         {
             get { return grid[x, y]; }
         }
 
+        /// <summary>
+        /// Gets the width of the heat map (the number of cells).
+        /// </summary>
         public int Width
         {
             get { return width; }
         }
+        /// <summary>
+        /// Gets the height of the heat map (the number of cells).
+        /// </summary>
         public int Height
         {
             get { return height; }
         }
 
+        /// <summary>
+        /// Gets the total number of cells in the heat map.
+        /// </summary>
         public int CellCount
         {
             get { return width * height; }
