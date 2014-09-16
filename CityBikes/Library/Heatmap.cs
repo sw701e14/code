20398ff9b2b514262573d7ecac0262c62c582cb3
deaddog <mikkelslsl@gmail.com>
2014-09-16 11:18:07 +0200

    Created a method in the heatmap for constructing a map from an array of integers.

--- CityBikes/Library/Heatmap.cs
+++ CityBikes/Library/Heatmap.cs
@@ -56,5 +56,44 @@ namespace Library
         {
             get { return width * height; }
         }
+
+        /// <summary>
+        /// Constructs a <see cref="Heatmap"/> from an array of integers by comparing its values to those in the min-max range.
+        /// </summary>
+        /// <param name="data">The data that should be translated into a <see cref="Heatmap"/>.</param>
+        /// <param name="min">The minimum value of values in the <see cref="Heatmap"/>. Anything below this value is set to <c>0</c>.</param>
+        /// <param name="max">The maximum value of values in the <see cref="Heatmap"/>. Anything above this value is set to <c>1</c>.</param>
+        /// <returns>A new <see cref="Heatmap"/> constructed from <paramref name="data"/>.</returns>
+        public static Heatmap ConstructByCount(int[,] data, int min, int max)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            if (data.Rank != 2)
+                throw new ArgumentException("The data for the map must be a two-dimensional array.", "data");
+
+            int w = data.GetLength(0);
+            int h = data.GetLength(1);
+
+            if (w <= 0)
+                throw new ArgumentOutOfRangeException("data", "Heatmap width must be >= 1.");
+            if (h <= 0)
+                throw new ArgumentOutOfRangeException("data", "Heatmap height must be >= 1.");
+
+            double mi = min;
+            double ma = max - min;
+
+            Heatmap map = new Heatmap(w, h);
+            for (int y = 0; y < h; y++)
+                for (int x = 0; x < w; x++)
+                {
+                    double d = data[x, y] - mi;
+                    if (d < 0) d = 0;
+                    else if (d > ma) d = ma;
+                    map.grid[x, y] = d / ma;
+                }
+
+            return map;
+        }
     }
 }
