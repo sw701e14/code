a4e1b288f4c8ac980cfde7ec646f31ac2502d71b
deaddog <mikkelslsl@gmail.com>
2014-09-16 11:15:25 +0200

    Added a width and a height field to the heat map. These are exposed through properties.

--- CityBikes/Library/Heatmap.cs
+++ CityBikes/Library/Heatmap.cs
@@ -4,9 +4,10 @@ namespace Library
 {
     public class Heatmap
     {
-        private double[,] grid;
+        private readonly int width, height;
+        private readonly double[,] grid;
 
-        public Heatmap(int width, int height)
+        private Heatmap(int width, int height)
         {
             if (width <= 0)
                 throw new ArgumentOutOfRangeException("width", "Heatmap width must be >= 1.");
@@ -14,12 +15,28 @@ namespace Library
             if (height <= 0)
                 throw new ArgumentOutOfRangeException("width", "Heatmap height must be >= 1.");
 
+            this.width = width;
+            this.height = height;
             this.grid = new double[width, height];
         }
 
         public double this[int x, int y]
         {
             get { return grid[x, y]; }
+        }
+
+        public int Width
+        {
+            get { return width; }
+        }
+        public int Height
+        {
+            get { return height; }
+        }
+
+        public int CellCount
+        {
+            get { return width * height; }
         }
     }
 }
