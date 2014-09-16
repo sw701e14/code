aabd2770fd12603c67ae080fed0c34394a1a6ce4
deaddog <mikkelslsl@gmail.com>
2014-09-16 10:53:07 +0200

    Implemented simple heatmap structure.

--- CityBikes/Library/Heatmap.cs
+++ CityBikes/Library/Heatmap.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library
 {
     public class Heatmap
     {
+        private double[,] grid;
+
+        public Heatmap(int width, int height)
+        {
+            if (width <= 0)
+                throw new ArgumentOutOfRangeException("width", "Heatmap width must be >= 1.");
+
+            if (height <= 0)
+                throw new ArgumentOutOfRangeException("width", "Heatmap height must be >= 1.");
+
+            this.grid = new double[width, height];
+        }
+
+        public double this[int x, int y]
+        {
+            get { return grid[x, y]; }
+        }
     }
 }
