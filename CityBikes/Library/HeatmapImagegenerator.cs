6604e3ab8d2de688588e1970e445a29513d6e19b
deaddog <mikkelslsl@gmail.com>
2014-09-18 09:46:22 +0200

    Implemented a Generate method in HeatmapImagegenerator that handles the generation of images.

--- CityBikes/Library/HeatmapImagegenerator.cs
+++ CityBikes/Library/HeatmapImagegenerator.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Library
 {
     public static class HeatmapImagegenerator
     {
+        public static Image Generate(Heatmap map, int imageWidth, int imageHeight, Func<double, Color> getColor)
+        {
+            Bitmap bmp = new Bitmap(imageWidth, imageHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                double w = (double)imageWidth / (double)map.Width;
+                double h = (double)imageHeight / (double)map.Height;
+
+                for (int y = 0; y < map.Height; y++)
+                    for (int x = 0; x < map.Width; x++)
+                    {
+                        using (SolidBrush brush = new SolidBrush(getColor(map[x, y])))
+                            g.FillRectangle(brush, (float)(x * w), (float)(y * h), (float)w, (float)h);
+                    }
+            }
+
+            return bmp;
+        }
     }
 }
