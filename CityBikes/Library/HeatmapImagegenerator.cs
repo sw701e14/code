2c82568d47c60732b666bbcd2f5f4b63acc302fa
deaddog <mikkelslsl@gmail.com>
2014-09-18 10:16:16 +0200

    The Generate method allows null as width/height argument.

--- CityBikes/Library/HeatmapImagegenerator.cs
+++ CityBikes/Library/HeatmapImagegenerator.cs
@@ -12,11 +12,11 @@ namespace Library
         /// Generates an image that represents a given <see cref="Heatmap"/> using a userdefined color-rule.
         /// </summary>
         /// <param name="map">The <see cref="Heatmap"/> from which the image should be generated.</param>
-        /// <param name="imageWidth">Width of the image (in pixels).</param>
-        /// <param name="imageHeight">Height of the image (in pixels).</param>
+        /// <param name="imageWidth">Width of the image (in pixels). If <c>null</c> is specified, the generated image will have the same width as the number of cells in the <see cref="Heatmap"/>.</param>
+        /// <param name="imageHeight">Height of the image (in pixels). If <c>null</c> is specified, the generated image will have the same height as the number of cells in the <see cref="Heatmap"/>.</param>
         /// <param name="getColor">A method that, given the weight of a single cell, returns the color of the cell.</param>
         /// <returns>The generated <see cref="Heatmap"/>.</returns>
-        public static Image Generate(Heatmap map, int imageWidth, int imageHeight, Func<double, Color> getColor)
+        public static Image Generate(Heatmap map, int? imageWidth, int? imageHeight, Func<double, Color> getColor)
         {
             if (map == null)
                 throw new ArgumentNullException("map");
@@ -30,12 +30,17 @@ namespace Library
             if (imageHeight <= 0)
                 throw new ArgumentOutOfRangeException("imageHeight", "Image height must be >= 1.");
 
-            Bitmap bmp = new Bitmap(imageWidth, imageHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            if (!imageWidth.HasValue)
+                imageWidth = map.Width;
+            if (!imageHeight.HasValue)
+                imageHeight = map.Height;
+
+            Bitmap bmp = new Bitmap(imageWidth.Value, imageHeight.Value, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
             using (Graphics g = Graphics.FromImage(bmp))
             {
-                double w = (double)imageWidth / (double)map.Width;
-                double h = (double)imageHeight / (double)map.Height;
+                double w = (double)imageWidth.Value / (double)map.Width;
+                double h = (double)imageHeight.Value / (double)map.Height;
 
                 for (int y = 0; y < map.Height; y++)
                     for (int x = 0; x < map.Width; x++)
