c2d61efe5b45130ce377449bab74666b8d4fdf7c
Bruno Thalmann <mr.thalmann@gmail.com>
2014-09-19 13:13:25 +0200

    added test about AM and PM conversion is happening correct

--- DatabaseImport/DatabaseImportTest/CSVParserTests.cs
+++ DatabaseImport/DatabaseImportTest/CSVParserTests.cs
@@ -12,16 +12,15 @@ namespace Library.Tests
     {
         GPSPoint gps1;
         GPSPoint gps2;
-        GPSPoint gps3;
-        GPSPoint[] data = new GPSPoint[1];
+        GPSPoint[] data = new GPSPoint[2];
 
         [TestInitialize]
         public void Setup()
         {
             gps1 = new GPSPoint(new DateTime(2014, 9, 12, 11, 34, 03), 57.01162, 9.99120, 49, 1);
-            gps2 = new GPSPoint(new DateTime(2014, 9, 12, 01, 54, 02), 57.01194, 9.99110, 16, 1);
-            gps3 = new GPSPoint(new DateTime(2014, 9, 12, 02, 14, 02), 57.02801, 9.94940, 16, 1);
+            gps2 = new GPSPoint(new DateTime(2014, 9, 12, 23, 34, 03), 57.01162, 9.99120, 49, 1);
             data[0] = gps1;
+            data[1] = gps2;
         }
 
         [TestMethod()]
@@ -31,6 +30,15 @@ namespace Library.Tests
             GPSPoint[] expected = data;
             
             Assert.AreEqual(expected[0].TimeStamp, actual[0].TimeStamp);
+        }
+
+        [TestMethod()]
+        public void GetDataTestTimeStampPM()
+        {
+            GPSPoint[] actual = CSVParser.GetData("test.txt", 1);
+            GPSPoint[] expected = data;
+
+            Assert.AreEqual(expected[1].TimeStamp, actual[1].TimeStamp);
         }
 
         [TestMethod()]
