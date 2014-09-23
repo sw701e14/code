800f1b4e9a1290129068eebb639f33f238d69c96
deaddog <mikkelslsl@gmail.com>
2014-09-23 09:40:54 +0200

    SQLExportTests uses the new testing scheme.

--- DatabaseImport/DatabaseImportTest/SQLExportTests.cs
+++ DatabaseImport/DatabaseImportTest/SQLExportTests.cs
@@ -20,31 +20,24 @@ namespace DatabaseImportTest
             gps1 = new GPSPoint(new DateTime(2014, 1, 1, 12, 0, 0), 57.01163, 9.99110, 10, 1);
             gps2 = new GPSPoint(new DateTime(2014, 1, 1, 12, 30, 09), 57.03106, 9.94580, 30, 1);
             gps3 = new GPSPoint(new DateTime(2014, 3, 13, 23, 24, 23), 57.01271, 9.98780, null, 1);
-
         }
-
-
 
         [TestMethod()]
         public void writeGPSPointTest1()
         {
-            var method = typeof(SQLExport).GetMethod("writeGPSPoint");
-            string output = method.Invoke(null, new object[] { gps1 }) as string;
+            string output = typeof(SQLExport).InvokeStaticPrivate<string>("writeGPSPoint", gps1);
             string expected = "('2014-01-01 12:00:00', 57.01163, 9.9911, 10, 1)";
 
             Assert.IsTrue(expected.SequenceEqual(output));
-
         }
 
         [TestMethod()]
         public void writeGPSPointTest2()
         {
-            var method = typeof(SQLExport).GetMethod("writeGPSPoint");
-            string output = method.Invoke(null, new object[] { gps2 }) as string;
+            string output = typeof(SQLExport).InvokeStaticPrivate<string>("writeGPSPoint", gps2);
             string expected = "('2014-01-01 12:30:09', 57.03106, 9.9458, 30, 1)";
 
             Assert.IsTrue(expected.SequenceEqual(output));
-
         }
     }
 }
