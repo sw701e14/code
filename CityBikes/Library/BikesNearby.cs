497e36576a0716b69ddd3b9c1819189dd482d82d
Bruno Thalmann <mr.thalmann@gmail.com>
2014-09-23 11:35:35 +0200

    added file for issue

---
+++ CityBikes/Library/BikesNearby.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.GeneratedDatabaseModel;
+
+namespace Library
+{
+    public static class BikesNearby
+    {
+        public static string GetBikesNearby()
+        {
+            citybike_testEntities1 context = new citybike_testEntities1();
+            var query = from b in context.gps_data
+                        orderby b.bikeId
+                        select b;
+
+            foreach (gps_data g in query)
+            {
+                return g.accuracy.ToString();
+            }
+            return "abekat";
+        }
+        
+        
+    }
+}
