956bf93eeb08825237d7255f3bcf819f940e4f14
stufkan <stufkan@gmail.com>
2014-09-26 10:36:31 +0200

    Changed GetBikeLocation to work as inteded. Closes #8

--- CityBikes/Library/AllBikesLocation.cs
+++ CityBikes/Library/AllBikesLocation.cs
@@ -10,36 +10,34 @@ namespace Library
     public class AllBikesLocation
     {
         Database context = new Database();
-        public gps_data GetBikeLocation(int Id)
+
+
+        /// <summary>
+        /// Gets the location of the bike with <para>Id</para>.
+        /// </summary>
+        /// <param name="Id">The id of the bike to find the location of.</param>
+        /// <returns>The GPSLocation of the bike with <para>id</para></returns>
+        public GPSLocation GetBikeLocation(int Id)
         {
             var location = (from bike in context.gps_data where bike.bikeId == Id orderby bike.queried descending select bike);
 
-            return location.First();
+            return location.First().Location;
         }
 
+        /// <summary>
+        /// Gets the latest location of all bikes
+        /// </summary>
+        /// <returns>An IEnumerable containing a Tuple for each bike with its bikeId and its location </returns>
         public IEnumerable<Tuple<int, GPSLocation>> GetBikeLocations()
         {
             return from bike in context.gps_data
-                     group bike by bike.bikeId into b
-                     let newestLocation = b.Max(x => x.queried)
+                   group bike by bike.bikeId into b
+                   let newestLocation = b.Max(x => x.queried)
 
-                     from g in b
-                     where g.queried == newestLocation
-                     select  Tuple.Create(g.bikeId, g.Location);
-
-            
+                   from g in b
+                   where g.queried == newestLocation
+                   select Tuple.Create(g.bikeId, g.Location);
         }
 
-        public IEnumerable<int> findAllIds()
-        {
-            return from bike in context.gps_data group bike by bike.bikeId into b select b.Key;
-        }
-
-        public gps_data FindLocationsOfAllBikes(IQueryable bikeIds)
-        {
-            var bikes = findAllIds();
-
-            return new gps_data();
-        }
     }
 }
