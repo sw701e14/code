42a33745e99251676fa3c332cf07b0aba832837f
deaddog <mikkelslsl@gmail.com>
2014-09-26 09:24:00 +0200

    Added a static class for implementation of #12.

---
+++ CityBikes/Library/BikeStandstill.cs
@@ -0,0 +1,19 @@
+﻿using Library.GeneratedDatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class BikeStandstill
+    {
+        private static Database database = new Database();
+
+        public static IEnumerable<Tuple<int, DateTime>> GetBikesImmobile()
+        {
+            
+        }
+    }
+}
