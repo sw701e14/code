c4389ae21ddd1e2ae6a3d5af4b3fbec84943a628
stufkan <stufkan@gmail.com>
2014-09-26 10:54:10 +0200

    test of getBikeLocation

--- CityBikes/LibraryTests/BikesNearbyTests.cs
+++ CityBikes/LibraryTests/BikesNearbyTests.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Library;
+using Library.GeneratedDatabaseModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Library.Tests
 {
     [TestClass()]
     public class BikesNearbyTests
     {
+        Database context = new Database();
+
         [TestMethod()]
         public void GetBikesNearbyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetBikeLocationTest()
+        {
+            AllBikesLocation abl = new AllBikesLocation();
+            GPSLocation loc = abl.GetBikeLocation(65530);
+
+            GPSLocation expected = new GPSLocation(4.12345678m,-4.12345678m);
+            Assert.AreEqual(expected, loc);
+        }
+
+        [TestMethod()]
+        public void GetBikeLocationsTest()
         {
             Assert.Fail();
         }
