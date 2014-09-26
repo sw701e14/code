ab56b89f37c7f9b50d84321b94f942d076e6656f
deaddog <mikkelslsl@gmail.com>
2014-09-26 12:13:14 +0200

    Added the gps_data partial class again.

---
+++ CityBikes/Library/GeneratedDatabaseModel/gps_data.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.GeneratedDatabaseModel
+{
+    public partial class gps_data
+    {
+        public GPSLocation Location
+        {
+            get { return new GPSLocation(this.latitude, this.longitude); }
+            set { this.latitude = value.Latitude; this.longitude = value.Longitude; }
+        }
+    }
+}
