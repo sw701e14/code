d4622f207c209f1ffafadd55c1dd5f87c24b1ae0
Draegert <alexander_draegert@hotmail.com>
2014-09-26 13:59:51 +0200

    Refactored GetAvailableBikes to use other methods.

--- CityBikes/Library/AvailableBikes.cs
+++ CityBikes/Library/AvailableBikes.cs
@@ -9,21 +9,25 @@ namespace Library
 {
     public static class AvailableBikes
     {
-        public static IEnumerable<Tuple<int/*, GPSLocation*/>> GetAvailableBikes()
+        public static IEnumerable<Tuple<int, GPSLocation>> GetAvailableBikes()
         {
-            IEnumerable<Tuple<int, GPSLocation>> allBikesPosition; /*= FindLocationOfAllBikes();*/
-            IEnumerable<Tuple<int, DateTime>> allBikesImmobile; /*= BikesImmobileFor();*/
+            IEnumerable<Tuple<int, GPSLocation>> allBikesPosition = FindLocationOfAllBikes();
+            IEnumerable<Tuple<int, DateTime>> allBikesImmobile = AllBikesImmobileFor();
+            IEnumerable<Tuple<int, GPSLocation>> allStations = FindAllSations();
 
+            var allBikes = allBikesPosition.Join(allBikesImmobile, p => p.Item1, i => i.Item1, (p, i) => new {tuple = Tuple.Create(p.Item1, p.Item2, i.Item2)});
 
-            IEnumerable<Tuple<int, DateTime, GPSLocation>> answer = allBikesPosition.Join(allBikesImmobile, p => p.Item1, i => i.Item1, (p, i) => new {Tuple<p.Item1, p.Item2, i.Item2});
-
-
-
-            foreach (Tuple<int/*, GPSLocation*/> bike in allBikesPosition)
+            foreach (var bike in allBikes)
             {
-                if (true)
+                if (AtAStation(bike.tuple.Item2))
                 {
-                    yield return bike;
+                    if (bike.tuple.Item3 > AVAILABLE_AFTER_AT_STATION)
+                        yield return Tuple.Create(bike.tuple.Item1, bike.tuple.Item2);
+                }
+                else
+                {
+                    if (bike.tuple.Item3 > AVAILABLE_AFTER_NOT_AT_STATION)
+                        yield return Tuple.Create(bike.tuple.Item1, bike.tuple.Item2);
                 }
             }
 
