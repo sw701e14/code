3e31f841c02f624496aa962fe7106e80ed24ee9f
deaddog <mikkelslsl@gmail.com>
2014-10-13 09:53:07 +0200

    Implemented GetBikesImmobilem such that the most-recent data is returned for each bike.

--- CityBikes/Library/BikeStandstill.cs
+++ CityBikes/Library/BikeStandstill.cs
@@ -11,13 +11,47 @@ namespace Library
     {
         private static Database database = new Database();
 
+        /// <summary>
+        /// Gets a collection of all bikes and a <see cref="DateTime"/> value indicating when they were "parked".
+        /// </summary>
+        /// <returns>A collection of bikes and park-times.</returns>
         public static IEnumerable<Tuple<int, DateTime>> GetBikesImmobile()
         {
+            Dictionary<int, gps_data> firstData = new Dictionary<int, gps_data>();
+            Dictionary<int, gps_data> lastData = new Dictionary<int, gps_data>();
+            List<int> stopped = new List<int>();
 
+            foreach (var v in from gps in database.gps_data orderby gps.queried select gps)
+            {
+                if (stopped.Contains(v.bikeId))
+                    continue;
+
+                if (!lastData.ContainsKey(v.bikeId))
+                {
+                    firstData.Add(v.bikeId, v);
+                    lastData.Add(v.bikeId, v);
+                }
+                else
+                {
+                    if (inVicinity(firstData[v.bikeId], v))
+                        lastData[v.bikeId] = v;
+                    else
+                        stopped.Add(v.bikeId);
+                }
+            }
+
+            foreach (var v in lastData)
+                yield return Tuple.Create(v.Key, v.Value.queried);
         }
         public static IEnumerable<Tuple<int, DateTime>> GetBikesImmobile(DateTime immobileSince)
         {
             return GetBikesImmobile().Where(b => b.Item2 < immobileSince);
+        }
+
+        private static bool inVicinity(gps_data d1, gps_data d2)
+        {
+            double dist = GPSLocation.Distance(d1.Location, d2.Location) * 1000;
+            return d1.accuracy + d2.accuracy >= dist;
         }
     }
 }
