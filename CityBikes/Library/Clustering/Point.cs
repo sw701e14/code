ef9283ab6601c71aaa1f45540b4506a87ff03865
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-23 09:58:47 +0200

    moved clustering and added classes for points used in DBSCAN

---
+++ CityBikes/Library/Clustering/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.GeneratedDatabaseModel;
+
+namespace Library.Clustering
+{
+    public abstract class Point
+    {
+        public Point() { }
+
+        private GPSLocation location;
+
+        public GPSLocation Location
+        {
+            get { return location; }
+        }
+        
+    }
+}
