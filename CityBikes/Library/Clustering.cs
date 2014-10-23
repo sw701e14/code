400b0bdd20d22667d177d3ddba5ef73fa44db5fc
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-23 09:34:43 +0200

    defined signature for dbscan

--- CityBikes/Library/Clustering.cs
+++ CityBikes/Library/Clustering.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.GeneratedDatabaseModel;
 
 namespace Library
 {
-    public class Clustering
+    public static class Clustering
     {
+        public GPSLocation[,] DBSCAN(int minimumPoints, double radius)
+        {
+
+            return null;
+        }
     }
 }
