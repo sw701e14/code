9219ea0e445e98918d651ee6a099d1f1462ee5b2
stufkan <stufkan@gmail.com>
2014-10-23 10:27:08 +0200

    test of the grahamscan in the convexhull class

---
+++ CityBikes/LibraryTests/ConvexHullTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using Library.GeneratedDatabaseModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Library.Tests
+{
+    [TestClass()]
+    public class ConvexHullTests
+    {
+        GPSLocation[] locations;
+        
+        [TestInitialize]
+        public void setup()
+        {
+            locations = new GPSLocation[] { new GPSLocation(1, 1), new GPSLocation(10, 1), new GPSLocation(10, 10), new GPSLocation(1, 10), new GPSLocation(5, 5), new GPSLocation(3, 7), new GPSLocation(4, 2), new GPSLocation(7, 3), new GPSLocation(15, 5), new GPSLocation(5,15) , new GPSLocation(10,1)};
+        }
+
+        [TestMethod()]
+        public void GrahamScanTest()
+        {
+            GPSLocation[] actual = ConvexHull.GrahamScan(locations);
+
+            GPSLocation[] expected = new GPSLocation[] { new GPSLocation(1, 1), new GPSLocation(1, 10), new GPSLocation(15, 5), new GPSLocation(10, 1) ,new GPSLocation(5,15)};
+
+            Assert.AreEqual(actual.Count(), expected.Count());
+
+            foreach (var expectedItem in expected)
+            {
+                if(!actual.Contains(expectedItem))
+                    Assert.Fail("Actual does not contain the expected item");
+            }
+        }
+    }
+}
