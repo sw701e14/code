2bcefced93bcbf63858fbeed2c06854013ed9f41
mikaelec <mikaelec@gmail.com>
2014-10-27 16:13:07 +0100

    Added overall logic for inserting location

--- CityBikes/Library/BikeUpdateLocation.cs
+++ CityBikes/Library/BikeUpdateLocation.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.GeneratedDatabaseModel;
 
 namespace DatabaseImport
 {
     public static class BikeUpdateLocation
     {
+        public static int insertLocation(gps_data newLocation)
+        {
+            Database db = new Database();
 
+            gps_data latestLocation = db.gps_data.Where(x => x.id == newLocation.id).OrderBy(x => x.queried).FirstOrDefault();
+
+            if (latestLocation != null && gps_data.WithinAccuracy(newLocation, latestLocation))
+                latestLocation.hasNotMoved = true;
+            else
+                db.gps_data.AddObject(newLocation);
+
+            return db.SaveChanges();
+        }
     }
 }
