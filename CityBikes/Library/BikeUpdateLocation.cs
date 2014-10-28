1217deb254e0f571f600d03f16d27ef97104df5e
mikaelec <mikaelec@gmail.com>
2014-10-28 11:02:01 +0100

    Added documentation

--- CityBikes/Library/BikeUpdateLocation.cs
+++ CityBikes/Library/BikeUpdateLocation.cs
@@ -9,18 +9,25 @@ namespace Library
 {
     public static class BikeUpdateLocation
     {
-        public static int InsertLocation(gps_data newLocation)
+        /// <summary>
+        /// Inserts new gps_data into database, depending on whether the bike has moved or not
+        /// </summary>
+        /// <param name="newLocation">The updated location to be inserted</param>
+        public static void InsertLocation(gps_data newLocation)
         {
             Database db = new Database();
 
-            gps_data latestLocation = db.gps_data.Where(x => x.bikeId == newLocation.bikeId).OrderByDescending(x => x.queried).FirstOrDefault();
+            gps_data latestLocation = db.gps_data
+                .Where(x => x.bikeId == newLocation.bikeId)
+                .OrderByDescending(x => x.queried)
+                .FirstOrDefault();
 
             if (latestLocation != null && gps_data.WithinAccuracy(newLocation, latestLocation))
                 latestLocation.hasNotMoved = true;
             else
                 db.gps_data.AddObject(newLocation);
 
-            return db.SaveChanges();
+            db.SaveChanges();
         }
     }
 }
