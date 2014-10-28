c9f1923a76ceb9c5a167f4c696fd3f23d3f1a558
deaddog <mikkelslsl@gmail.com>
2014-10-28 11:03:28 +0100

    BikesNearby uses the Distance method from GPSLocation.

--- CityBikes/Library/BikesNearby.cs
+++ CityBikes/Library/BikesNearby.cs
@@ -17,7 +17,7 @@ namespace Library
         /// <returns>Returns a list of bike id and their location.</returns>
         public static IEnumerable<Tuple<int, GPSLocation>> GetBikesNearby(GPSLocation gpsLocation)
         {
-            Dictionary<int, Tuple<decimal, DateTime, GPSLocation>> bikes = new Dictionary<int, Tuple<decimal, DateTime, GPSLocation>>();
+            Dictionary<int, Tuple<double, DateTime, GPSLocation>> bikes = new Dictionary<int, Tuple<double, DateTime, GPSLocation>>();
             Database context = new Database();
             var query =  from bike in context.gps_data
                          group bike by bike.bikeId into b
@@ -29,10 +29,10 @@ namespace Library
 
             foreach (gps_data g in query)
             {
-                bikes.Add(g.bikeId, Tuple.Create(GPSTools.GetDistance(gpsLocation.Latitude, gpsLocation.Longitude, g.latitude, g.longitude), g.queried, new GPSLocation(g.latitude, g.longitude)));
+                bikes.Add(g.bikeId, Tuple.Create(gpsLocation.DistanceTo(g.Location), g.queried, new GPSLocation(g.latitude, g.longitude)));
             }
 
-            foreach (KeyValuePair<int, Tuple<decimal, DateTime, GPSLocation>> bike in bikes.OrderBy(x => x.Value.Item1))
+            foreach (KeyValuePair<int, Tuple<double, DateTime, GPSLocation>> bike in bikes.OrderBy(x => x.Value.Item1))
             {
                 yield return Tuple.Create(bike.Key, bike.Value.Item3);
             }
