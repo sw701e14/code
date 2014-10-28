21093685953f2a8cbbdd2e8d229e2a80c3449a34
deaddog <mikkelslsl@gmail.com>
2014-10-28 09:43:29 +0100

    Implemented the DatabaseExport class.

--- CityBikes/DatabaseImport/DatabaseExport.cs
+++ CityBikes/DatabaseImport/DatabaseExport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.GeneratedDatabaseModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace DatabaseImport
 {
     public static class DatabaseExport
     {
+        /// <summary>
+        /// Exports the specified points directly to the database.
+        /// </summary>
+        /// <param name="database">The database to which the point should be exported.</param>
+        /// <param name="points">The points that should be exported.</param>
+        public static void Export(Database database, IEnumerable<gps_data> points)
+        {
+            foreach (var p in points)
+                database.gps_data.AddObject(p);
+        }
     }
 }
