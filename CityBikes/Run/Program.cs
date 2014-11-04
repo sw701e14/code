31ce61fc52367deadc8be86188d6ca939a916648
deaddog <mikkelslsl@gmail.com>
2014-11-04 11:22:47 +0100

    Removed the random test-stuff from program.cs.

--- CityBikes/Run/Program.cs
+++ CityBikes/Run/Program.cs
@@ -12,25 +12,6 @@ namespace Run
     {
         static void Main(string[] args)
         {
-            //For testing the bikesnearby method..
-            /*
-            //Console.WriteLine("BikesNearby:");
-            foreach (var item in BikesNearby.GetBikesNearby(new GPSLocation(2,-2)))
-            {
-                Console.WriteLine("bikeID: " + item.Item1 + " Distance to given point: " + item.Item2.Latitude + ", " + item.Item2.Longitude);
-            }
-
-            Console.WriteLine("AvailableBikes:");
-
-            foreach (int bikeID in AvailableBikes.GetAvailableBikes())
-            {
-                Console.WriteLine("bikeID: " + bikeID);
-            }
-
-            Console.ReadKey();
-             */
-            //testBikeStandstill();
-            //testAvailableBikes();
         }
     }
 }
