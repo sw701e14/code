883a2dec14305230f8606e2acf5a5cbd44d415d1
stufkan <stufkan@gmail.com>
2014-11-07 09:50:02 +0100

    Markov chain model class

---
+++ CityBikes/Library/MarkovChain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class MarkovChain
+    {
+        private double[,] markovChain;
+        private double[] initialState;
+
+        public double this[int i, int j]
+        {
+            get { return markovChain[i, j]; }
+            set { markovChain[i, j] = value; }
+        }
+
+        public MarkovChain(int size, double[] initialState)
+        {
+            markovChain = new double[size,size];
+            this.initialState = initialState;
+        }
+    }
+}
