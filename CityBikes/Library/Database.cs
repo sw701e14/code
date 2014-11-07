85ec3eda6465c4498d590baff9dfc4995d110b80
deaddog <mikkelslsl@gmail.com>
2014-11-07 13:16:12 +0100

    Created a RowCollection class that uses a nested IEnumerator<Row> class to handle command execution.

--- CityBikes/Library/Database.cs
+++ CityBikes/Library/Database.cs
@@ -66,6 +66,71 @@ namespace Library
             }
         }
 
+        public class RowCollection : IEnumerable<Row>
+        {
+            private readonly string command;
+            private readonly MySqlConnection connection;
+
+            public RowCollection(string command, MySqlConnection connection)
+            {
+                this.command = command;
+                this.connection = connection;
+            }
+
+            IEnumerator<Row> IEnumerable<Row>.GetEnumerator()
+            {
+                return new Enumerator(new MySqlCommand(command, connection));
+            }
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return new Enumerator(new MySqlCommand(command, connection));
+            }
+
+            private class Enumerator : IEnumerator<Row>
+            {
+                private MySqlCommand command;
+
+                private Row row;
+                private MySqlDataReader reader;
+
+                public Enumerator(MySqlCommand command)
+                {
+                    if (command == null)
+                        throw new ArgumentNullException("command");
+
+                    this.command = command;
+
+                    this.row = null;
+                    this.reader = null;
+                }
+
+                public Row Current
+                {
+                    get { throw new NotImplementedException(); }
+                }
+
+                public void Dispose()
+                {
+                    throw new NotImplementedException();
+                }
+
+                object System.Collections.IEnumerator.Current
+                {
+                    get { throw new NotImplementedException(); }
+                }
+
+                public bool MoveNext()
+                {
+                    throw new NotImplementedException();
+                }
+
+                public void Reset()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
         public class Row
         {
             private MySqlDataReader reader;
