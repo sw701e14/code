af8e9885ca97113661c6daa1282340eca34a74fd
deaddog <mikkelslsl@gmail.com>
2014-11-07 10:04:31 +0100

    Implemented Database setup and Dispose.

--- CityBikes/Library/Database.cs
+++ CityBikes/Library/Database.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    public class Database
+    public class Database : IDisposable
     {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string username;
+        private string password;
+
+        public Database()
+        {
+            Initialize();
+        }
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "citybike_test";
+            username = "root";
+            password = "takecare";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+        void IDisposable.Dispose()
+        {
+            connection.Dispose();
+        }
     }
 }
