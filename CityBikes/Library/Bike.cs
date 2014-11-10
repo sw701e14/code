5859fe42e3670cefe27d894d02369e09c2e26a57
deaddog <mikkelslsl@gmail.com>
2014-11-10 13:29:29 +0100

    Created a Bike class as part of the database model.

---
+++ CityBikes/Library/Bike.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Bike
+    {
+        private long id;
+
+        public Bike(long id)
+        {
+            this.id = id;
+        }
+
+        public long Id
+        {
+            get { return id; }
+        }
+    }
+}
