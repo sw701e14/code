4e0fa0bb71bb3ce28737aa41424e332ea7cdcc49
deaddog <mikkelslsl@gmail.com>
2014-11-11 09:47:27 +0100

    Rewrote AllBikesLocation using the new database scheme.

--- CityBikes/Library/AllBikesLocation.cs
+++ CityBikes/Library/AllBikesLocation.cs
@@ -3,44 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Library.GeneratedDatabaseModel;
 
 namespace Library
 {
     public static class AllBikesLocation
     {
         /// <summary>
-        /// Gets the location of the bike with <paramref name="Id"/>.
+        /// Gets the location of the bike with <paramref name="id"/>.
         /// </summary>
-        /// <param name="context">A database context from which data should be retrieved.</param>
-        /// <param name="Id">The id of the bike to find the location of.</param>
-        /// <returns>The GPSLocation of the bike with <paramref name="Id"/> or null if var location is null</returns>
-        public static GPSLocation GetBikeLocation(this Database context, long Id)
+        /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
+        /// <param name="id">The id of the bike to find the location of.</param>
+        /// <returns>The GPSLocation of the bike with <paramref name="id"/>.</returns>
+        public static GPSLocation GetBikeLocation(this Database.DatabaseSession session, long id)
         {
-            var location = (from bike in context.gps_data where bike.bikeId == Id orderby bike.queried descending select bike);
+            var rows = session.ExecuteRead(
+@"SELECT latitude, longitude
+FROM citybike_test.gps_data
+WHERE bikeId = {0}
+ORDER BY queried desc", id);
 
-            return location.FirstOrDefault().Location;
+            return rows.First().GetValue<GPSLocation>();
         }
 
         /// <summary>
         /// Gets the latest location of all bikes
         /// </summary>
-        /// <param name="context">A database context from which data should be retrieved.</param>
-        /// <returns>An IEnumerable containing a Tuple for each bike with its bikeId and its location </returns>
-        public static IEnumerable<Tuple<long, GPSLocation>> GetBikeLocations(this Database context)
+        /// <param name="session">A database context from which data should be retrieved.</param>
+        /// <returns>An array of Tuples containing a Bike and its location </returns>
+        public static Tuple<Bike, GPSLocation>[] GetBikeLocations(this Database.DatabaseSession session)
         {
-            var latest = from bike in context.gps_data
-                         group bike by bike.bikeId into b
-                         let newestLocation = b.Max(x => x.queried)
+            var rows = session.ExecuteRead(
+@"SELECT g1.bikeId, latitude, longitude
+FROM citybike_test.gps_data g1
+INNER JOIN (
+    SELECT bikeId, MAX(queried) as MaxDate
+    FROM citybike_test.gps_data
+    GROUP BY bikeId
+) g2 ON g1.bikeId = g2.bikeId AND g1.queried = g2.MaxDate");
 
-                         from g in b
-                         where g.queried == newestLocation
-                         select g;
-
-            foreach (var item in latest)
-            {
-                yield return Tuple.Create(item.bikeId, item.Location);
-            }
+            return rows.Select(row => row.ToTuple<Bike, GPSLocation>()).ToArray();
         }
     }
 }
