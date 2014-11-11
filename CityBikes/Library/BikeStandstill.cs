1b51523267164aafe80f2e11346f400668959a7f
deaddog <mikkelslsl@gmail.com>
2014-11-11 09:53:48 +0100

    Removed the nullables from BikeStandStill and updated the sql.

--- CityBikes/Library/BikeStandstill.cs
+++ CityBikes/Library/BikeStandstill.cs
@@ -13,16 +13,18 @@ namespace Library
         /// </summary>
         /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
         /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
-        public static Tuple<Bike, DateTime?, bool?>[] GetBikesImmobile(this Database.DatabaseSession session)
+        public static Tuple<Bike, DateTime, bool>[] GetBikesImmobile(this Database.DatabaseSession session)
         {
             var rows = session.ExecuteRead(
-@"SELECT bikes.id, MAX(queried), hasNotMoved
-FROM citybike_test.bikes
-LEFT OUTER JOIN citybike_test.gps_data
-ON citybike_test.bikes.id = citybike_test.gps_data.bikeId
-GROUP BY bikes.id");
+@"SELECT g1.bikeId, queried, hasNotMoved
+FROM citybike_test.gps_data g1
+INNER JOIN (
+    SELECT bikeId, MAX(queried) as MaxDate
+    FROM citybike_test.gps_data
+    GROUP BY bikeId
+) g2 ON g1.bikeId = g2.bikeId AND g1.queried = g2.MaxDate");
 
-            return rows.Select(row => row.ToTuple<Bike, DateTime?, bool?>()).ToArray();
+            return rows.Select(row => row.ToTuple<Bike, DateTime, bool>()).ToArray();
         }
         /// <summary>
         /// Gets a collection of all bikes and a <see cref="DateTime"/> value indicating when they were "parked".
@@ -30,7 +32,7 @@ GROUP BY bikes.id");
         /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
         /// <param name="immobileSince">Any bikes that were parked after <paramref name="immobileSince"/> will not be returned.</param>
         /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
-        public static Tuple<Bike, DateTime?, bool?>[] GetBikesImmobile(this Database.DatabaseSession session, DateTime immobileSince)
+        public static Tuple<Bike, DateTime, bool>[] GetBikesImmobile(this Database.DatabaseSession session, DateTime immobileSince)
         {
             return GetBikesImmobile(session).Where(b => b.Item2 < immobileSince).ToArray();
         }
