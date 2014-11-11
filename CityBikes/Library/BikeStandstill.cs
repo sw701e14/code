616aa384c93db911d992292889df7ba5c291a18c
deaddog <mikkelslsl@gmail.com>
2014-11-11 07:40:49 +0100

    Moved the GPSLocation class to the Library root.

--- CityBikes/Library/BikeStandstill.cs
+++ CityBikes/Library/BikeStandstill.cs
@@ -1,5 +1,4 @@
-﻿using Library.GeneratedDatabaseModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +13,7 @@ namespace Library
         /// </summary>
         /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
         /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
-        public static Tuple<Bike, DateTime?, bool>[] GetBikesImmobile(this Database.DatabaseSession session)
+        public static Tuple<Bike, DateTime?, bool?>[] GetBikesImmobile(this Database.DatabaseSession session)
         {
             var rows = session.ExecuteRead(
 @"SELECT bikes.id, MAX(queried), hasNotMoved
@@ -23,7 +22,7 @@ LEFT OUTER JOIN citybike_test.gps_data
 ON citybike_test.bikes.id = citybike_test.gps_data.bikeId
 GROUP BY bikes.id");
 
-            return rows.Select(row => row.ToTuple<Bike, DateTime?, bool>()).ToArray();
+            return rows.Select(row => row.ToTuple<Bike, DateTime?, bool?>()).ToArray();
         }
         /// <summary>
         /// Gets a collection of all bikes and a <see cref="DateTime"/> value indicating when they were "parked".
@@ -31,7 +30,7 @@ GROUP BY bikes.id");
         /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
         /// <param name="immobileSince">Any bikes that were parked after <paramref name="immobileSince"/> will not be returned.</param>
         /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
-        public static Tuple<Bike, DateTime?, bool>[] GetBikesImmobile(this Database.DatabaseSession session, DateTime immobileSince)
+        public static Tuple<Bike, DateTime?, bool?>[] GetBikesImmobile(this Database.DatabaseSession session, DateTime immobileSince)
         {
             return GetBikesImmobile(session).Where(b => b.Item2 < immobileSince).ToArray();
         }
--- CityBikes/Library/GPSLocation.cs
+++ CityBikes/Library/GPSLocation.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Library.GeneratedDatabaseModel
+namespace Library
 {
     [Serializable]
     public struct GPSLocation : IEquatable<GPSLocation>
