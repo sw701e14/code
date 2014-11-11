2a79d554a9453502ba3be6a72bd44e02ee4d4a03
mikaelec <mikaelec@gmail.com>
2014-11-11 10:01:53 +0100

    Added bikes controller

---
+++ CityBikes/Webservice/Controllers/BikeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Library;
+using Library.GeneratedDatabaseModel;
+
+namespace Webservice.Controllers
+{
+    [RoutePrefix("bikes")]
+    public class BikeController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public Tuple<long, GPSLocation>[] getAll()
+        {
+            Database context = new Database();
+
+            return AvailableBikes.GetAvailableBikes(context).ToArray();
+        }
+    }
+}
