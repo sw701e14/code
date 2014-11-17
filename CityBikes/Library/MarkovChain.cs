aa733d867ff37908426da6e762bbbfff4601a802
stufkan <stufkan@gmail.com>
2014-11-17 14:35:41 +0100

    implementation of CreateChain which creates a markov chain of the provided frequencies

--- CityBikes/Library/MarkovChain.cs
+++ CityBikes/Library/MarkovChain.cs
@@ -8,17 +8,47 @@ namespace Library
 {
     public class MarkovChain
     {
-        private double[,] markovChain;
+        double[,] markovChain;
+        private double[,] markovFrequencies;
 
         public double this[int i, int j]
         {
-            get { return markovChain[i, j]; }
-            set { markovChain[i, j] = value; }
+            get { return markovFrequencies[i, j]; }
+            set { markovFrequencies[i, j] = value; }
         }
 
         public MarkovChain(int size)
         {
-            markovChain = new double[size,size];
+            markovFrequencies = new double[size, size];
+            markovChain = new double[size, size];
         }
+
+        private int Sum()
+        {
+            int k = 0;
+            foreach (var item in markovFrequencies)
+            {
+                k += (int)item;
+            }
+            return k;
+        }
+
+
+        /// <summary>
+        /// Creates the markov chain from the frequencies inputted and saves it in the MarkovChain
+        /// </summary>
+        public void CreateChain()
+        {
+            int s = Sum();
+
+            for (int i = 0; i < markovFrequencies.GetLength(0); i++)
+            {
+                for (int j = 0; j < markovFrequencies.GetLength(1); j++)
+                {
+                    markovChain[i, j] = markovFrequencies[i, j] / s;
+                }
+            }
+        }
+
     }
 }
