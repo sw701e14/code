2a03b382389a342b86ca7185410e448c882fce5f
deaddog <mikkelslsl@gmail.com>
2014-11-24 11:23:13 +0100

    Data can no longer be loaded from CSV files using the import program.

--- CityBikes/DatabaseImport/Program.cs
+++ CityBikes/DatabaseImport/Program.cs
@@ -33,59 +33,9 @@ namespace Run
 
         static void loadData()
         {
-            if (!hasFiles())
-                return;
-
-            Menu<GPSData[]> menu = new Menu<GPSData[]>("Load data!");
-            menu.SetCancel("Done");
-
-            menu.Add("Load from file", loadFromFile);
-            menu.Add("Load from Google Directions", loadFromGoogle);
-
-            foreach (var set in menu.Show(true))
-            {
-                if (set != null)
-                    using (Database database = new Database())
-                        database.RunSession(session => DatabaseExport.Export(session, set));
-            }
-        }
-
-        static bool hasFiles()
-        {
-            if (!Directory.EnumerateFiles(".", "*.txt").Any())
-            {
-                Console.WriteLine("Unable to find any .txt files in working directory.");
-                Console.WriteLine("Make sure that your projects working directory,");
-                Console.WriteLine("is set to the directory with the test-files.");
-                Console.WriteLine();
-                Console.WriteLine("The current working dir is:");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("    {0}\\", Path.GetFullPath("."));
-                Console.ForegroundColor = ConsoleColor.Gray;
-
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("Press any key to exit application.");
-                Console.ReadKey(true);
-                return false;
-            }
-            return true;
-        }
-
-        static GPSData[] loadFromFile()
-        {
-            Menu<GPSData[]> menu = new Menu<GPSData[]>("Select a file:");
-            menu.SetCancel("Cancel");
-
-            foreach (var f in Directory.EnumerateFiles(".", "*.txt"))
-                menu.Add(Path.GetFileName(f), () => loadFromFile(f));
-
-            return menu.Show();
-        }
-        static GPSData[] loadFromFile(string path)
-        {
-            uint id = (uint)"Specify bike ID: ".GetInt32(x => x >= 0);
-            return CSVParser.GetData(path, new Bike(id));
+            var points = loadFromGoogle();
+            using (Database database = new Database())
+                database.RunSession(session => DatabaseExport.Export(session, points));
         }
 
         static GPSData[] loadFromGoogle()
