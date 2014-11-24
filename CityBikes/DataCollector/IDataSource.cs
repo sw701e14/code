73c188f113c0d5044b0f6f8a805a02c92c9fa3e7
deaddog <mikkelslsl@gmail.com>
2014-11-24 14:33:05 +0100

    Created an interface that supports asking for new gps data, as per #32.

---
+++ CityBikes/DataCollector/IDataSource.cs
@@ -0,0 +1,16 @@
+﻿using Library;
+
+namespace DataCollector
+{
+    /// <summary>
+    /// Defines methods used to retrieve <see cref="GPSData"/> from a data source.
+    /// </summary>
+    public interface IDataSource
+    {
+        /// <summary>
+        /// Gets data from the data source, if available.
+        /// </summary>
+        /// <returns>The oldest <see cref="GPSData"/> available at the data source; <c>null</c> if no data is available.</returns>
+        GPSData? GetData();
+    }
+}
