1d5ee871994abffc3d7edf4fb98b48a6761ba1ee
deaddog <mikkelslsl@gmail.com>
2014-11-24 15:26:11 +0100

    Created a GoogleDataSource that will use the google API to query for locations.

---
+++ CityBikes/DataCollector/GoogleDataSource.cs
@@ -0,0 +1,24 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    public class GoogleDataSource : IDataSource
+    {
+        private Bike bike;
+
+        public GoogleDataSource(Bike bike)
+        {
+            this.bike = bike;
+        }
+
+        public GPSData? GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
