1471b7b167e8bf6675bd06bd4163ad52fd5b7c4f
deaddog <mikkelslsl@gmail.com>
2014-11-24 15:48:48 +0100

    Created a wrapper class for route interval conversion.

---
+++ CityBikes/DataCollector/IntervalConverter.cs
@@ -0,0 +1,14 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    public class IntervalConverter
+    {
+        
+    }
+}
