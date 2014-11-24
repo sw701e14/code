b75313e3abf8ec0f94d1bb8650e2e27f8d417d67
deaddog <mikkelslsl@gmail.com>
2014-11-24 15:24:49 +0100

    Provided documentation for the MultiDataSource.

--- CityBikes/DataCollector/MultiDataSource.cs
+++ CityBikes/DataCollector/MultiDataSource.cs
@@ -7,21 +7,38 @@ using System.Threading.Tasks;
 
 namespace DataCollector
 {
+    /// <summary>
+    /// Retrives <see cref="GPSData"/> from a collection of data sources by querying each of them one at a time.
+    /// </summary>
     public class MultiDataSource : IDataSource
     {
         private IDataSource[] sources;
         private int index = 0;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiDataSource"/> class.
+        /// </summary>
+        /// <param name="sources">A collection of the sources that should be queried by this <see cref="MultiDataSource"/>.</param>
         public MultiDataSource(IEnumerable<IDataSource> sources)
             : this(sources.ToArray())
         {
         }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiDataSource"/> class.
+        /// </summary>
+        /// <param name="sources">An array containing the sources that should be queried by this <see cref="MultiDataSource"/>.</param>
         public MultiDataSource(params IDataSource[] sources)
         {
             this.sources = new IDataSource[sources.Length];
             sources.CopyTo(this.sources, 0);
         }
 
+        /// <summary>
+        /// Gets data from one of the data sources managed by this <see cref="MultiDataSource"/>, if available.
+        /// </summary>
+        /// <returns>
+        /// The oldest <see cref="GPSData" /> available at the data source; <c>null</c> if no data is available.
+        /// </returns>
         public GPSData? GetData()
         {
             GPSData? data = null;
