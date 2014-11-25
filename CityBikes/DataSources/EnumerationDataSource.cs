91ee2d201c92d202af0ee11159bfc9957404ad5c
deaddog <mikkelslsl@gmail.com>
2014-11-25 09:11:17 +0100

    Implemented a class that converts IEnumeration into a DataSource.

---
+++ CityBikes/DataSources/EnumerationDataSource.cs
@@ -0,0 +1,32 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+
+namespace DataSources
+{
+    public class EnumerationDataSource : IDataSource
+    {
+        private IEnumerable<GPSData> data;
+        private IEnumerator<GPSData> e;
+
+        public EnumerationDataSource(IEnumerable<GPSData> data)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            this.data = data;
+            this.e = this.data.GetEnumerator();
+        }
+
+        public GPSData? GetData()
+        {
+            if (e.MoveNext())
+                return e.Current;
+            else
+            {
+                e.Dispose();
+                return null;
+            }
+        }
+    }
+}
