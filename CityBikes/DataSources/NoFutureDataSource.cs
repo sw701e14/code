be6ebf86eb07c6d00c0d080a9425dff92123bc2e
deaddog <mikkelslsl@gmail.com>
2014-11-25 10:12:13 +0100

    Created a data source that ensures that data is only 'loaded' if it is new.

---
+++ CityBikes/DataSources/NoFutureDataSource.cs
@@ -0,0 +1,41 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSources
+{
+    public class NoFutureDataSource : IDataSource
+    {
+        private IDataSource source;
+        private GPSData? data;
+
+        public NoFutureDataSource(IDataSource source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source");
+
+            this.source = source;
+            this.data = null;
+        }
+
+        public GPSData? GetData()
+        {
+            if (!data.HasValue)
+                data = source.GetData();
+
+            if (!data.HasValue)
+                return null;
+            else if (data.Value.QueryTime > DateTime.Now)
+                return null;
+            else
+            {
+                var d = data;
+                data = null;
+                return d;
+            }
+        }
+    }
+}
