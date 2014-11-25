896a5a632cc7fe49e0d8dcee8924db5f33700c99
deaddog <mikkelslsl@gmail.com>
2014-11-25 09:50:05 +0100

    Moved GenerateGPSData to the DataSources project.

--- CityBikes/DataSources/GenerateGPSData.cs
+++ CityBikes/DataSources/GenerateGPSData.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 
-namespace DatabaseImport
+namespace DataSources
 {
     public static class GenerateGPSData
     {
--- CityBikes/DatabaseImport/Program.cs
+++ CityBikes/DatabaseImport/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using DatabaseImport;
+using DataSources;
+using DeadDog.Console;
+using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DatabaseImport;
-using DeadDog.Console;
-using System.IO;
-using Library;
-using System.Diagnostics;
 
 namespace Run
 {
