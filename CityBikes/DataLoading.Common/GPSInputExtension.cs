cf6bd45c353ed314876d09fca2ff2a78c91e7f1e
deaddog <mikkelslsl@gmail.com>
2014-12-01 13:36:20 +0100

    Rewrote the GPSInputExtension to use the new GPSInput type definition.

--- CityBikes/DataLoading.Common/GPSInputExtension.cs
+++ CityBikes/DataLoading.Common/GPSInputExtension.cs
@@ -3,65 +3,66 @@ using System.Collections.Generic;
 
 namespace DataLoading.Common
 {
-   public static class GPSInputExtension
-   {
-       private static Random r = new Random();
-       
-       public static IEnumerable<GPSData> ConvertToInterval(this IEnumerable<GPSData> route, TimeSpan interval)
-       {
-           if (route == null)
-               throw new ArgumentNullException("route");
+    public static class GPSInputExtension
+    {
+        private static Random r = new Random();
 
-           if (interval.TotalSeconds <= 0.0)
-               throw new ArgumentOutOfRangeException("interval", "The interval used for conversion must be strictly positive.");
+        public static IEnumerable<GPSInput> ConvertToInterval(this IEnumerable<GPSInput> route, TimeSpan interval)
+        {
+            if (route == null)
+                throw new ArgumentNullException("route");
 
-           var e = route.GetEnumerator();
+            if (interval.TotalSeconds <= 0.0)
+                throw new ArgumentOutOfRangeException("interval", "The interval used for conversion must be strictly positive.");
 
-           if (!e.MoveNext())
-           {
-               e.Dispose();
-               yield break;
-           }
+            var e = route.GetEnumerator();
 
-           Bike bike = e.Current.Bike;
+            if (!e.MoveNext())
+            {
+                e.Dispose();
+                yield break;
+            }
 
-           var lastPoint = e.Current;
-           yield return lastPoint;
-           DateTime nextTime = lastPoint.QueryTime.Add(interval).addRandomSeconds(60);
+            uint bike = e.Current.BikeId;
 
-           while (e.MoveNext())
-           {
-               var nextPoint = e.Current;
+            var lastPoint = e.Current;
+            yield return lastPoint;
+            DateTime nextTime = lastPoint.Timestamp.Add(interval).addRandomSeconds(60);
 
-               while (nextPoint.QueryTime >= nextTime)
-               {
-                   var point = generateBetweenPoint(lastPoint, nextPoint, nextTime);
-                   yield return new GPSData(bike, point, null, nextTime);
-                   nextTime = nextTime.Add(interval).addRandomSeconds(60);
-               }
+            while (e.MoveNext())
+            {
+                var nextPoint = e.Current;
 
-               lastPoint = nextPoint;
-           }
+                while (nextPoint.Timestamp >= nextTime)
+                {
+                    var point = generateBetweenPoint(lastPoint, nextPoint, nextTime);
+                    yield return new GPSInput(bike, point.Item1, point.Item2, null, nextTime);
+                    nextTime = nextTime.Add(interval).addRandomSeconds(60);
+                }
 
-           e.Dispose();
-       }
-       private static DateTime addRandomSeconds(this DateTime dt, double secondsRange)
-       {
-           return dt.AddSeconds((r.NextDouble() - 0.5) * secondsRange);
-       }
-       private static GPSLocation generateBetweenPoint(GPSData lp, GPSData np, DateTime time)
-       {
-           var diff = (np.QueryTime - lp.QueryTime);
+                lastPoint = nextPoint;
+            }
 
-           double triptime = diff.TotalSeconds;
-           double pointtime = (time - lp.QueryTime).TotalSeconds;
+            e.Dispose();
+        }
 
-           if (pointtime != 0)
-               return lp.Location + (np.Location - lp.Location) * (pointtime / triptime);
-           else
-               return lp.Location;
-       }
+        private static DateTime addRandomSeconds(this DateTime dt, double secondsRange)
+        {
+            return dt.AddSeconds((r.NextDouble() - 0.5) * secondsRange);
+        }
+        private static Tuple<decimal, decimal> generateBetweenPoint(GPSInput lp, GPSInput np, DateTime time)
+        {
+            var diff = (np.Timestamp - lp.Timestamp);
 
+            double triptime = diff.TotalSeconds;
+            double pointtime = (time - lp.Timestamp).TotalSeconds;
 
+            if (pointtime != 0)
+                return Tuple.Create(
+                     (decimal)((double)lp.Latitude + (double)(np.Latitude - lp.Latitude) * (pointtime / triptime)),
+                     (decimal)((double)lp.Longitude + (double)(np.Longitude - lp.Longitude) * (pointtime / triptime)));
+            else
+                return Tuple.Create(lp.Latitude, lp.Longitude);
+        }
     }
 }
