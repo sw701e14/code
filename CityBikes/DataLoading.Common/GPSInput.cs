a47148b75a297c3e20e39b29bfe1a0f145de8720
deaddog <mikkelslsl@gmail.com>
2014-12-01 11:20:40 +0100

    Created a struct that will represent data returned from a data source.

---
+++ CityBikes/DataLoading.Common/GPSInput.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLoading.Common
+{
+    public struct GPSInput
+    {
+        private static Random rnd = new Random();
+        private static byte getAccuracy(byte? accuracy)
+        {
+            if (!accuracy.HasValue)
+                return (byte)rnd.Next(30);
+            else
+                return accuracy.Value;
+        }
+
+        private uint bikeId;
+        private decimal latitude, longitude;
+        private byte accuracy;
+        private DateTime queryTime;
+
+        public GPSInput(uint bikeId, decimal latitude, decimal longitude, byte? accuracy, DateTime queryTime)
+        {
+            this.bikeId = bikeId;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.accuracy = getAccuracy(accuracy);
+            this.queryTime = queryTime;
+        }
+
+        public uint BikeId
+        {
+            get { return bikeId; }
+        }
+        public decimal Latitude
+        {
+            get { return latitude; }
+        }
+        public decimal Longitude
+        {
+            get { return longitude; }
+        }
+        public byte Accuracy
+        {
+            get { return accuracy; }
+        }
+        public DateTime QueryTime
+        {
+            get { return queryTime; }
+        }
+    }
+}
