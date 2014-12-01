51ae5e738a371ac3ed2b20a79db16959aa3afd63
deaddog <mikkelslsl@gmail.com>
2014-12-01 11:29:45 +0100

    Provided documentation for the GPSInput class.

--- CityBikes/DataLoading.Common/GPSInput.cs
+++ CityBikes/DataLoading.Common/GPSInput.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace DataLoading.Common
 {
-    public struct GPSInput
+    /// <summary>
+    /// Represents data that is retrieved from a GPS unit.
+    /// </summary>
+    public class GPSInput
     {
         private static Random rnd = new Random();
         private static byte getAccuracy(byte? accuracy)
@@ -20,36 +23,59 @@ namespace DataLoading.Common
         private uint bikeId;
         private decimal latitude, longitude;
         private byte accuracy;
-        private DateTime queryTime;
+        private DateTime timestamp;
 
-        public GPSInput(uint bikeId, decimal latitude, decimal longitude, byte? accuracy, DateTime queryTime)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GPSInput"/> class.
+        /// </summary>
+        /// <param name="bikeId">The identifier for the bike that this GPS data is associated with.</param>
+        /// <param name="latitude">The latitude component of the GPS data.</param>
+        /// <param name="longitude">The longitude component of the GPS data.</param>
+        /// <param name="accuracy">The accuracy of the GPS measurement (in meters). Setting this value to <c>null</c> will result in a random accuracy.</param>
+        /// <param name="timestamp">The time at which the GPS data was recorded.</param>
+        public GPSInput(uint bikeId, decimal latitude, decimal longitude, byte? accuracy, DateTime timestamp)
         {
             this.bikeId = bikeId;
             this.latitude = latitude;
             this.longitude = longitude;
             this.accuracy = getAccuracy(accuracy);
-            this.queryTime = queryTime;
+            this.timestamp = timestamp;
         }
 
+        /// <summary>
+        /// Gets the identifier for the bike that this GPS data is associated with.
+        /// </summary>
         public uint BikeId
         {
             get { return bikeId; }
         }
+        /// <summary>
+        /// Gets the latitude component of the GPS data.
+        /// </summary>
         public decimal Latitude
         {
             get { return latitude; }
         }
+        /// <summary>
+        /// Gets the longitude component of the GPS data.
+        /// </summary>
         public decimal Longitude
         {
             get { return longitude; }
         }
+        /// <summary>
+        /// Gets the accuracy of the GPS measurement (in meters).
+        /// </summary>
         public byte Accuracy
         {
             get { return accuracy; }
         }
-        public DateTime QueryTime
+        /// <summary>
+        /// Gets the time at which the GPS data was recorded.
+        /// </summary>
+        public DateTime Timestamp
         {
-            get { return queryTime; }
+            get { return timestamp; }
         }
     }
 }
