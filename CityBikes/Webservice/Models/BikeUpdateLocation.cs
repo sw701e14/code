759cc67a4d7ea0d0e0a8540c4f58788b796ab8ea
Stefan Micheelsen <stufkan@gmail.com>
2014-12-04 11:26:53 +0100

    setHasNotMoved sql moved to dal

--- CityBikes/Webservice/Models/BikeUpdateLocation.cs
+++ CityBikes/Webservice/Models/BikeUpdateLocation.cs
@@ -34,7 +34,7 @@ namespace Webservice.Model
             if (GPSData.WithinAccuracy(newLocation, latestLocation))
             {
                 if (!latestLocation.HasNotMoved)
-                    setHasNotMoved(session, newLocation.Bike);
+                    setHasNotMoved(newLocation.Bike);
             }
             else
             {
@@ -42,23 +42,11 @@ namespace Webservice.Model
             }
         }
 
-        
-        private static void setHasNotMoved(Database.DatabaseSession session, Bike bike)
+        private static void setHasNotMoved(Bike bike)
         {
-            session.Execute(
-@"UPDATE gps_data a
-INNER JOIN
-(
-    SELECT  bikeId, MAX(id) id
-    FROM    gps_data
-    GROUP   BY bikeId
-) b ON  a.id = b.id AND {0} = b.bikeId
-SET a.hasNotMoved = '1'", bike.Id);
-
+            Shared.DAL.UpdateQueries.setHasNotMoved(bike);
             var old = bikesWithKnownLastLocation[bike];
             bikesWithKnownLastLocation[bike] = new GPSData(bike, old.Location, old.Accuracy, old.QueryTime, true);
         }
-
-        
     }
 }
