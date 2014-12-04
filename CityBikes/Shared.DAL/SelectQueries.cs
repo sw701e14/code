1a534322baa7ebaa869916b479ffe7ebf5a39067
Stefan Micheelsen <stufkan@gmail.com>
2014-12-04 10:56:37 +0100

    bikesimmobile to DAL

--- CityBikes/Shared.DAL/SelectQueries.cs
+++ CityBikes/Shared.DAL/SelectQueries.cs
@@ -71,5 +71,34 @@ INNER JOIN (
         {
             return Database.RunCommand(session => session.ExecuteRead("SELECT bikeId, latitude, longitude, accuracy, queried, hasNotMoved FROM citybike_test.gps_data WHERE bikeId = {0} ORDER BY queried DESC", bikeId).Any());
         }
+
+        /// <summary>
+        /// Gets a collection of all bikes and a <see cref="DateTime"/> value indicating when they were "parked".
+        /// </summary>
+        /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
+        /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
+        public static Tuple<Bike, DateTime, bool>[] GetBikesImmobile()
+        {
+            var rows = Database.RunCommand(session=>session.ExecuteRead(
+@"SELECT g1.bikeId, queried, hasNotMoved
+FROM citybike_test.gps_data g1
+INNER JOIN (
+    SELECT bikeId, MAX(queried) as MaxDate
+    FROM citybike_test.gps_data
+    GROUP BY bikeId
+) g2 ON g1.bikeId = g2.bikeId AND g1.queried = g2.MaxDate"));
+
+            return rows.Select(row => row.ToTuple<Bike, DateTime, bool>()).ToArray();
+        }
+        /// <summary>
+        /// Gets a collection of all bikes and a <see cref="DateTime"/> value indicating when they were "parked".
+        /// </summary>
+        /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
+        /// <param name="immobileSince">Any bikes that were parked after <paramref name="immobileSince"/> will not be returned.</param>
+        /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
+        public static Tuple<Bike, DateTime, bool>[] GetBikesImmobile(DateTime immobileSince)
+        {
+            return GetBikesImmobile().Where(b => b.Item2 < immobileSince).ToArray();
+        }
     }
 }
