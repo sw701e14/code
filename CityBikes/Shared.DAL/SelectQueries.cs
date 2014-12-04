d96d9c52bf2e2e9ab4e7c60e0f52f0751b590781
Stefan Micheelsen <stufkan@gmail.com>
2014-12-04 10:21:19 +0100

    selectmethods for gpsdata,markov and available bikes in DAL

--- CityBikes/Shared.DAL/SelectQueries.cs
+++ CityBikes/Shared.DAL/SelectQueries.cs
@@ -13,19 +13,54 @@ namespace Shared.DAL
 
         public GPSData LatestGPSData(Bike b)
         {
-            Database db = new Database();
-            GPSData gps = db.RunSession<GPSData>(session => session.ExecuteRead("SELECT bikeId, latitude, longitude, accuracy, queried, hasNotMoved FROM citybike_test.gps_data WHERE bikeId = {0} ORDER BY queried DESC", b.Id).First().GetGPSData());
-            db.Dispose();
-            return gps;
+            return Database.RunCommand(session => session.ExecuteRead("SELECT bikeId, latitude, longitude, accuracy, queried, hasNotMoved FROM citybike_test.gps_data WHERE bikeId = {0} ORDER BY queried DESC", b.Id).First().GetGPSData());
         }
 
         public byte[] AllMarkovChains(int column)
         {
-            Database db = new Database();
-            Database.RowCollection serializedMarkovChain = db.RunSession(session=>session.ExecuteRead("SELECT mc FROM markov_chains"));
+            var serializedMarkovChain = Database.RunCommand(session=>session.ExecuteRead("SELECT mc FROM markov_chains"));
             byte[] data = serializedMarkovChain.ElementAt(column).GetValue<byte[]>();
-            db.Dispose();
+            
             return data;
+        }
+
+        /// <summary>
+        /// Gets the location of the bike with <paramref name="id"/>.
+        /// </summary>
+        /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
+        /// <param name="id">The id of the bike to find the location of.</param>
+        /// <returns>The GPSLocation of the bike with <paramref name="id"/>.</returns>
+        public GPSLocation GetBikeLocation(long id)
+        {
+            var rows = Database.RunCommand(session=>
+            session.ExecuteRead(
+@"SELECT latitude, longitude
+FROM citybike_test.gps_data
+WHERE bikeId = {0}
+ORDER BY queried desc", id));
+            
+            return rows.First().GetValue<GPSLocation>();
+        }
+
+        /// <summary>
+        /// Gets the latest location of all bikes
+        /// </summary>
+        /// <param name="session">A <see cref="Database.DatabaseSession"/> from which data should be retrieved.</param>
+        /// <returns>An array of Tuples containing a Bike and its location </returns>
+        public Tuple<Bike, GPSLocation>[] GetBikeLocations()
+        {
+
+            var rows = Database.RunCommand(session => session.ExecuteRead(
+@"SELECT g1.bikeId, latitude, longitude
+FROM citybike_test.gps_data g1
+INNER JOIN (
+    SELECT bikeId, MAX(queried) as MaxDate
+    FROM citybike_test.gps_data
+    GROUP BY bikeId
+) g2 ON g1.bikeId = g2.bikeId AND g1.queried = g2.MaxDate"));
+
+            
+            return rows.Select(row => row.ToTuple<Bike, GPSLocation>()).ToArray();
         }
 
     }
