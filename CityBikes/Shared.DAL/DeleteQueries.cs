8467293d5ff2614ea24b02dfb294fb3900ce809e
Stefan Micheelsen <stufkan@gmail.com>
2014-12-04 11:32:11 +0100

    truncate all tables to dal

---
+++ CityBikes/Shared.DAL/DeleteQueries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DAL
+{
+    public static class DeleteQueries
+    {
+        public static void TruncateAll()
+        {
+           Database.RunCommand(session=>session.Execute("TRUNCATE citybike_test.gps_data; TRUNCATE citybike_test.bikes; TRUNCATE citybike_test.hotspots"));
+        }
+    }
+}
