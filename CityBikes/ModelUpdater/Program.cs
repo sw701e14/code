0bd13420b6f290b65fdf807234782fcfdd1d2c85
Draegert <alexander_draegert@hotmail.com>
2014-12-08 14:31:51 +0100

    Created console application program in ModelUpdater.

---
+++ CityBikes/ModelUpdater/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Shared.DTO;
+using Shared.DAL;
+
+namespace ModelUpdater
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
