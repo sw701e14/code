ab757404049d4bb1baa3240202c756896e47fe27
deaddog <mikkelslsl@gmail.com>
2014-12-08 16:09:12 +0100

    Cleaned the MarkovChain class.

--- CityBikes/ModelUpdater/MarkovChain.cs
+++ CityBikes/ModelUpdater/MarkovChain.cs
@@ -10,8 +10,7 @@ namespace ModelUpdater
 {
     public class MarkovChain
     {
-        double[,] markovChain;
-        private double[,] markovFrequencies;
+        private Matrix probabilities;
 
         public static Matrix BuildMarkovMatrix(Hotspot[] hotspots, GPSData[] data)
         {
@@ -53,6 +52,9 @@ namespace ModelUpdater
             }
             return new Matrix(counter);
         }
+
+        #region Methods and types related to building markov matrices
+
         private static IEnumerable<dataenumerator> groupData(GPSData[] data)
         {
             Array.Sort(data, buildSort);
@@ -122,49 +124,16 @@ namespace ModelUpdater
             }
         }
 
-        public double this[int i, int j]
+        #endregion
+
+        public MarkovChain(Matrix matrix)
         {
-            get { return markovFrequencies[i, j]; }
-            set { markovFrequencies[i, j] = value; }
+            this.probabilities = matrix;
         }
 
-        public MarkovChain(int size)
+        public Matrix Probabilities
         {
-            markovFrequencies = new double[size, size];
-            markovChain = new double[size, size];
-        }
-
-        private int Sum()
-        {
-            int k = 0;
-            foreach (var item in markovFrequencies)
-            {
-                k += (int)item;
-            }
-            return k;
-        }
-
-        public int size
-        {
-            get { return markovChain.GetLength(0); }
-        }
-
-
-
-        /// <summary>
-        /// Creates the markov chain from the frequencies inputted and saves it in the MarkovChain
-        /// </summary>
-        public void CreateChain()
-        {
-            int s = Sum();
-
-            for (int i = 0; i < markovFrequencies.GetLength(0); i++)
-            {
-                for (int j = 0; j < markovFrequencies.GetLength(1); j++)
-                {
-                    markovChain[i, j] = markovFrequencies[i, j] / s;
-                }
-            }
+            get { return probabilities; }
         }
     }
 }
