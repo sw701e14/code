8e57fcde5b01d4d146557e6a7e127a2171f75018
deaddog <mikkelslsl@gmail.com>
2014-12-09 17:23:21 +0100

    Provided documentation for the deletion queries.

--- CityBikes/Shared.DAL/DeleteQueries.cs
+++ CityBikes/Shared.DAL/DeleteQueries.cs
@@ -8,22 +8,38 @@ namespace Shared.DAL
 {
     public static class DeleteQueries
     {
+        /// <summary>
+        /// Truncates all tables in the database.
+        /// </summary>
+        /// <param name="session">A <see cref="DatabaseSession"/> using which data should be deleted.</param>
         public static void TruncateAll(this DatabaseSession session)
         {
             session.Execute("TRUNCATE citybike_test.gps_data; TRUNCATE citybike_test.bikes; TRUNCATE citybike_test.hotspots; TRUNCATE citybike_test.markov_chains");
         }
 
+        /// <summary>
+        /// Truncates the table with GPS data.
+        /// </summary>
+        /// <param name="session">A <see cref="DatabaseSession"/> using which data should be deleted.</param>
         public static void TruncateGPS_data(this DatabaseSession session)
         {
              session.Execute("TRUNCATE TABLE gps_data");
         }
 
+        /// <summary>
+        /// Truncates the table with markov chains.
+        /// </summary>
+        /// <param name="session">A <see cref="DatabaseSession"/> using which data should be deleted.</param>
         public static void TruncateMarkov_chains(this DatabaseSession session)
         {
             session.Execute("TRUNCATE citybike_test.markov_chains");
         }
 
-        public static void TruncateHotspots(this Database.DatabaseSession session)
+        /// <summary>
+        /// Truncates the table with hotspots.
+        /// </summary>
+        /// <param name="session">A <see cref="DatabaseSession"/> using which data should be deleted.</param>
+        public static void TruncateHotspots(this DatabaseSession session)
         {
             session.Execute("TRUNCATE citybike_test.hotspots");
         }
