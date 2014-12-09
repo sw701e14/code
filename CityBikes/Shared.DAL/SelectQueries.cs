83d09c20fa2f21a672499cb59b833dc554eb193e
deaddog <mikkelslsl@gmail.com>
2014-12-09 17:04:32 +0100

    Rewrote the GPSData related select queries.

--- CityBikes/Shared.DAL/SelectQueries.cs
+++ CityBikes/Shared.DAL/SelectQueries.cs
@@ -3,54 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Shared.DTO;
-
 
 namespace Shared.DAL
 {
     public static class SelectQueries
     {
-        public static GPSData? LatestGPSData(this DatabaseSession session, Bike b)
+        /// <summary>
+        /// Gets the last known GPS data for a single bike.
+        /// </summary>
+        /// <param name="session">A <see cref="DatabaseSession"/> from which data should be retrieved.</param>
+        /// <param name="id">The id of the bike to find the location of.</param>
+        /// <returns>A tuple with [latitude, longitude, accuracy, queried, hasNotMoved]</returns>
+        public static Tuple<decimal, decimal, byte, DateTime, bool> GetLastGPSData(this DatabaseSession session, uint bikeId)
         {
             var data = session.ExecuteRead(
-@"SELECT *
+@"SELECT latitude, longitude, accuracy, queried, hasNotMoved
 FROM citybike_test.gps_data
 WHERE bikeId = {0}
-ORDER BY queried DESC", b.Id).FirstOrDefault();
+ORDER BY queried DESC", bikeId).FirstOrDefault();
 
             if (data == null)
                 return null;
             else
-                return data.GetGPSData(1);
+                return data.ToTuple<decimal, decimal, byte, DateTime, bool>();
         }
-
         /// <summary>
-        /// Gets the location of the bike with <paramref name="id"/>.
+        /// Gets the last known location of all bikes.
         /// </summary>
         /// <param name="session">A <see cref="DatabaseSession"/> from which data should be retrieved.</param>
-        /// <param name="id">The id of the bike to find the location of.</param>
-        /// <returns>The GPSLocation of the bike with <paramref name="id"/>.</returns>
-        public static GPSLocation GetBikeLocation(this DatabaseSession session, long id)
+        /// <returns>An array of tuples with [bikeid, latitude, longitude, accuracy, queried, hasNotMoved]</returns>
+        public static Tuple<uint, decimal, decimal, byte, DateTime, bool>[] GetLastGPSData(this DatabaseSession session)
         {
             var rows = session.ExecuteRead(
-@"SELECT latitude, longitude
-FROM citybike_test.gps_data
-WHERE bikeId = {0}
-ORDER BY queried desc", id);
-
-            return rows.First().GetValue<GPSLocation>();
-        }
-
-        /// <summary>
-        /// Gets the latest location of all bikes
-        /// </summary>
-        /// <param name="session">A <see cref="DatabaseSession"/> from which data should be retrieved.</param>
-        /// <returns>An array of Tuples containing a Bike and its location </returns>
-        public static Tuple<Bike, GPSLocation>[] GetBikeLocations(this DatabaseSession session)
-        {
-
-            var rows = session.ExecuteRead(
-@"SELECT g1.bikeId, latitude, longitude
+@"SELECT g1.bikeId, latitude, longitude, accuracy, queried, hasNotMoved
 FROM citybike_test.gps_data g1
 INNER JOIN (
     SELECT bikeId, MAX(queried) as MaxDate
@@ -58,9 +43,35 @@ INNER JOIN (
     GROUP BY bikeId
 ) g2 ON g1.bikeId = g2.bikeId AND g1.queried = g2.MaxDate");
 
-
-            return rows.Select(row => row.ToTuple<Bike, GPSLocation>()).ToArray();
+            return rows.Select(row => row.ToTuple<uint, decimal, decimal, byte, DateTime, bool>()).ToArray();
         }
+        /// <summary>
+        /// Gets all GPS data in the database.
+        /// </summary>
+        /// <param name="session">A <see cref="DatabaseSession"/> from which data should be retrieved.</param>
+        /// <returns>An array of tuples with [bikeid, latitude, longitude, accuracy, queried, hasNotMoved]</returns>
+        public static Tuple<uint, decimal, decimal, byte, DateTime, bool>[] GetAllGPSData(this DatabaseSession session)
+        {
+            var rows = session.ExecuteRead(
+@"SELECT latitude, longitude, accuracy, queried, hasNotMoved
+FROM citybike_test.gps_data");
+
+            return rows.Select(row => row.ToTuple<uint, decimal, decimal, byte, DateTime, bool>()).ToArray();
+        }
+        /// <summary>
+        /// Gets all GPS data in the database where hasNotMoved = true.
+        /// </summary>
+        /// <param name="session">A <see cref="DatabaseSession"/> from which data should be retrieved.</param>
+        /// <returns>An array of tuples with [bikeid, latitude, longitude, accuracy, queried, hasNotMoved]</returns>
+        public static Tuple<uint, decimal, decimal, byte, DateTime, bool>[] GetAllGPSNotMovedData(this DatabaseSession session)
+        {
+            var rows = session.ExecuteRead(
+@"SELECT latitude, longitude, accuracy, queried, hasNotMoved 
+FROM citybike_test.gps_data Where hasNotMoved");
+
+            return rows.Select(row => row.ToTuple<uint, decimal, decimal, byte, DateTime, bool>()).ToArray();
+        }
+
 
         public static List<Hotspot> GetAllHotspots(this DatabaseSession session)
         {
@@ -104,16 +115,6 @@ INNER JOIN (
         public static Bike[] GetBikes(this DatabaseSession session)
         {
             return session.ExecuteRead("SELECT * FROM citybike_test.bikes").Select(row => row.GetBike()).ToArray();
-        }
-
-        public static GPSData[] GetAllGPSData(this DatabaseSession session)
-        {
-            return session.ExecuteRead("SELECT * FROM citybike_test.gps_data").Select(r => r.GetGPSData(1)).ToArray();
-        }
-
-        public static GPSData[] GetAllGPSNotMovedData(this DatabaseSession session)
-        {
-            return session.ExecuteRead("SELECT * FROM citybike_test.gps_data Where hasNotMoved").Select(r => r.GetGPSData(1)).ToArray();
         }
     }
 }
