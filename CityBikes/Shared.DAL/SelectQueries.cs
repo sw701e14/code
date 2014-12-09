571244b1588213bf4ec5dfd41862a3feeffe7391
deaddog <mikkelslsl@gmail.com>
2014-12-09 17:12:56 +0100

    Removed GetBikesImmobile; duplicate of GetLastGPSData.

--- CityBikes/Shared.DAL/SelectQueries.cs
+++ CityBikes/Shared.DAL/SelectQueries.cs
@@ -72,35 +72,6 @@ FROM citybike_test.gps_data Where hasNotMoved");
             return rows.Select(row => row.ToTuple<uint, decimal, decimal, byte, DateTime, bool>()).ToArray();
         }
 
-        /// <summary>
-        /// Gets a collection of all bikes and a <see cref="DateTime"/> value indicating when they were "parked".
-        /// </summary>
-        /// <param name="session">A <see cref="DatabaseSession"/> from which data should be retrieved.</param>
-        /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
-        public static Tuple<Bike, DateTime, bool>[] GetBikesImmobile(this DatabaseSession session)
-        {
-            var rows = session.ExecuteRead(
-@"SELECT g1.bikeId, queried, hasNotMoved
-FROM citybike_test.gps_data g1
-INNER JOIN (
-    SELECT bikeId, MAX(queried) as MaxDate
-    FROM citybike_test.gps_data
-    GROUP BY bikeId
-) g2 ON g1.bikeId = g2.bikeId AND g1.queried = g2.MaxDate");
-
-            return rows.Select(row => row.ToTuple<Bike, DateTime, bool>()).ToArray();
-        }
-        /// <summary>
-        /// Gets a collection of all bikes and a <see cref="DateTime"/> value indicating when they were "parked".
-        /// </summary>
-        /// <param name="session">A <see cref="DatabaseSession"/> from which data should be retrieved.</param>
-        /// <param name="immobileSince">Any bikes that were parked after <paramref name="immobileSince"/> will not be returned.</param>
-        /// <returns>A collection of bikes, last-use-time and a boolean indicating if their are standing still.</returns>
-        public static Tuple<Bike, DateTime, bool>[] GetBikesImmobile(this DatabaseSession session, DateTime immobileSince)
-        {
-            return GetBikesImmobile(session).Where(b => b.Item2 < immobileSince).ToArray();
-        }
-
         public static Bike[] GetBikes(this DatabaseSession session)
         {
             return session.ExecuteRead("SELECT * FROM citybike_test.bikes").Select(row => row.GetBike()).ToArray();
