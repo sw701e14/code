bb44b014ee1a9a58c333b40f05a29130dbb49be3
deaddog <mikkelslsl@gmail.com>
2014-12-11 19:40:16 +0100

    Added a class that provides a AddDistinct extension to IList.

---
+++ CityBikes/ModelUpdater/ListExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelUpdater
+{
+    public static class ListExtension
+    {
+        public static void AddDistinct<T>(this IList<T> list, IEnumerable<T> collection) where T : IEquatable<T>
+        {
+            foreach (var c in collection)
+                if (!list.Contains(c))
+                    list.Add(c);
+        }
+    }
+}
